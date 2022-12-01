416d9a02a3bab5ff83a43f2b59c554208c2582e9
WillisXoX <Williamstone4041@gmail.com>
2022-12-01 06:58:10 +0000

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ else:
 
 if r > 5:
     print(f"Last digit of {number:d} is {r:d} and is greater than 5")
-elif remainder == 0:
+elif r == 0:
     print(f"Last digit of {number:d} is {r:d} and is 0")
 else:
     print(f"Last digit of {number:d} is {r:d} and is less than 6 and not 0")
