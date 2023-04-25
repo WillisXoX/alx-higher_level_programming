e572bebcbcf006732491c9fa38507c7560b6964e
Willis <Williamstone4041@gmail.com>
2023-04-25 22:19:08 +0100

    100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -9,7 +9,7 @@ request(url, function (err, response, body) {
     console.log(err);
   } else if (response.statusCode === 200) {
     body = JSON.parse(body);
-    for (const i in body.characters) {
+    for (let i in body.characters) {
       request(body.characters[i], function (err, response, body) {
         if (err) {
           console.log(err);
